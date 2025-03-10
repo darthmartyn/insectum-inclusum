5c87b17e98eb7d097d042b66e6b3d28c9ac1b848
Martyn Pike <pike@adacore.com>
2025-03-10 08:20:47 +0000

    Update

--- build.ps1
+++ build.ps1
@@ -1,14 +1,41 @@
-param ([string[]]$rtspath = '', [string[]]$target = '')
+param (
+    [string[]]$rtspath = '',
+    [string[]]$target = '',
+    [string[]]$gccargs = '',
+    [string[]]$gpr = '',
+    [bool]$startupgen = $true
+)
 
-$gccargs = "-mlittle-endian -mthumb -mfloat-abi=hard -mcpu=cortex-m7 -mfpu=fpv5-sp-d16 -march=armv7e-m+fpv5"
+# Setup variables
+$objdir = Join-Path $PSScriptRoot "obj"
+$srcdir = Join-Path $PSScriptRoot "src"
+$libgnat = Join-Path $($rtspath) "adalib\libgnat.a"
+$bindsrc = "gnatmain.adb"
+$bindobj = "gnatmain.o"
+$image = "gnatimage.elf"
 
-$objdir = Join-Path $PWD "obj"
-$srcdir = Join-Path $PWD "src"
+# Pre-requisite checks on inputs
+if (!(Test-Path -Path $($libgnat)))
+{
+    Write-Host "RTS is missing libgnat.a";
+    exit 1
+}
+
+if (!(Test-Path -Path $($gpr)))
+{
+    Write-Host "Missing GPR file: $($gpr)";
+    exit 1
+}
 
 $alifiles = @()
 $objfiles = @()
 
-foreach ($obj in Get-ChildItem -Path $objdir\*.* -File -Recurse) { Remove-Item -Force -Path $($obj) }
+foreach ($obj in Get-ChildItem -Path $objdir\*.* -File -Recurse)
+{
+    Remove-Item -Force -Path $($obj)
+}
+
+Start-Sleep 1
 
 foreach ($src in Get-ChildItem -Path $srcdir\*.adb -File -Recurse)
 {
@@ -17,16 +44,16 @@ foreach ($src in Get-ChildItem -Path $srcdir\*.adb -File -Recurse)
     $adbfile = Join-Path $srcdir "$($src.BaseName).adb"
     Start-Process "$($target)-gcc" -ArgumentList "-g -c -o $($objfile) $($gccargs) --RTS=$($rtspath) $($adbfile)" -NoNewWindow -Wait
 
-    if (!(Test-Path -Path $($objfile)) -or !(Test-Path -Path $($alifile))) { exit 1 }
+    if (!(Test-Path -Path $($objfile)) -or !(Test-Path -Path $($alifile)))
+    {
+        exit 1
+    }
 
     $alifiles = $alifiles + $alifile
     $objfiles = $objfiles + $objfile
 }
 
-$bindsrc = "gnatbind.adb"
-$bindobj = "gnatbind.o"
-$image = "gnatbind.elf"
-
+# It's easier to work within the object directory, so let's move there
 Set-Location $objdir
 Start-Process "$($target)-gnatbind" -ArgumentList "-o $($bindsrc) --RTS=$($rtspath) $($alifiles) " -NoNewWindow -Wait
 
@@ -34,27 +61,65 @@ if (Test-Path -Path $($bindsrc))
 {
     Start-Process "$($target)-gcc" -ArgumentList "-g -c -o $($bindobj) $($gccargs) --RTS=$($rtspath) $($bindsrc)" -NoNewWindow -Wait
 
-    if (!(Test-Path -Path $($bindobj))) { exit 1 }
+    if (!(Test-Path -Path $($bindobj)))
+    {
+        Write-Host "Compilation of $($bindsrc) FAILED"; exit 1
+    }
 
     $objfiles = $objfiles + $bindobj
-    $libgnat = Join-Path $($rtspath) "adalib\libgnat.a"
-    Write-Host $PSScriptRoot
-    $gprfile = Get-ChildItem -Path $PSScriptRoot\*.gpr -File
+    $crt0obj = Join-Path $objdir "crt0.o"
 
-    if (!(Test-Path -Path $($gprfile))) { exit 1 }
+    if ($startupgen)
+    {
+        $linkerscript = Join-Path $objdir "linker.ld"
+        $crt0src = Join-Path $objdir "crt0.s"
 
-    $linkerscript = "linker.ld"
-    $crt0src = "crt0.s"
-    $crt0obj = "crt0.o"
+        Start-Process startup-gen -ArgumentList "-P $($gpr) -l $($linkerscript) -s $($crt0src)" -NoNewWindow -Wait
 
-    Start-Process startup-gen -ArgumentList "-P $($gprfile) -l $($linkerscript) -s $($crt0src)" -NoNewWindow -Wait
+        if (!(Test-Path -Path $($linkerscript)))
+        {
+            Write-Host "Generation of $($linkerscript) FAILED";
+            exit 1 
+        } else
+        {
+            if (!(Test-Path -Path $($crt0src)))
+            { 
+                Write-Host "Generation of $($crt0src) FAILED";
+                exit 1 
+            }
+        }
 
-    if (!(Test-Path -Path $($linkerscript)) -or (!(Test-Path -Path $($crt0src)))) { exit 1 }
+    } else
+    {
+        $linkerscript = Join-Path $srcdir "linker.ld"
+        if (!(Test-Path -Path $($linkerscript)))
+        {
+            Write-Host "$($linkerscript) is missing";
+            exit 1
+        } else
+        {
+            $crt0src = Join-Path $srcdir "crt0.s"
+            if (!(Test-Path -Path $($crt0src)))
+            {
+                Write-Host "$($crt0src) is missing";
+                exit 1
+            }        
+        }
+    }
 
     Start-Process "$($target)-as" -ArgumentList "-o $($crt0obj) $($gccargs) $($crt0src)" -NoNewWindow -Wait
     $objfiles = $objfiles + $($crt0obj)
 
+    if (!(Test-Path -Path $($crt0obj)))
+    {
+        Write-Host "Assembly of $($crt0src) FAILED";  exit 1
+    }
+
     Start-Process "$($target)-ld" -ArgumentList "-o $($image) $($objfiles) $($libgnat) -T $($linkerscript)" -NoNewWindow -Wait
 }
 
-Set-Location $PSScriptRoot 
+if (!(Test-Path -Path $($image))) { Write-Host "Linking of $($image) FAILED";  exit 1 }
+
+# A fully built target image is available from here onward...
+
+Set-Location $PSScriptRoot

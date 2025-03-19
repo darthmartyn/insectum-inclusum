ad9a25e34a7b842787b0aa610508703ab5245d15
Martyn Pike <pike@adacore.com>
2025-03-19 06:49:10 +0000

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -57,68 +57,69 @@ foreach ($src in Get-ChildItem -Path $srcdir\*.adb -File -Recurse)
 Set-Location $objdir
 Start-Process "$($target)-gnatbind" -ArgumentList "-o $($bindsrc) --RTS=$($rtspath) $($alifiles) " -NoNewWindow -Wait
 
-if (Test-Path -Path $($bindsrc))
+if (!(Test-Path -Path $($bindsrc)))
 {
-    Start-Process "$($target)-gcc" -ArgumentList "-g -c -o $($bindobj) $($gccargs) --RTS=$($rtspath) $($bindsrc)" -NoNewWindow -Wait
-
-    if (!(Test-Path -Path $($bindobj)))
-    {
-        Write-Host "Compilation of $($bindsrc) FAILED"; exit 1
-    }
-
-    $objfiles = $objfiles + $bindobj
-    $crt0obj = Join-Path $objdir "crt0.o"
-
-    if ($startupgen)
-    {
-        $linkerscript = Join-Path $objdir "linker.ld"
-        $crt0src = Join-Path $objdir "crt0.s"
-
-        Start-Process startup-gen -ArgumentList "-P $($gpr) -l $($linkerscript) -s $($crt0src)" -NoNewWindow -Wait
-
-        if (!(Test-Path -Path $($linkerscript)))
-        {
-            Write-Host "Generation of $($linkerscript) FAILED";
-            exit 1 
-        } else
-        {
-            if (!(Test-Path -Path $($crt0src)))
-            { 
-                Write-Host "Generation of $($crt0src) FAILED";
-                exit 1 
-            }
-        }
-
-    } else
-    {
-        $linkerscript = Join-Path $srcdir "linker.ld"
-        if (!(Test-Path -Path $($linkerscript)))
-        {
-            Write-Host "$($linkerscript) is missing";
-            exit 1
-        } else
-        {
-            $crt0src = Join-Path $srcdir "crt0.s"
-            if (!(Test-Path -Path $($crt0src)))
-            {
-                Write-Host "$($crt0src) is missing";
-                exit 1
-            }        
-        }
-    }
-
-    Start-Process "$($target)-as" -ArgumentList "-o $($crt0obj) $($gccargs) $($crt0src)" -NoNewWindow -Wait
-    $objfiles = $objfiles + $($crt0obj)
-
-    if (!(Test-Path -Path $($crt0obj)))
-    {
-        Write-Host "Assembly of $($crt0src) FAILED";  exit 1
-    }
-
-    Start-Process "$($target)-ld" -ArgumentList "-o $($image) $($objfiles) $($libgnat) -T $($linkerscript)" -NoNewWindow -Wait
+    Write-Host "Bind failed"; exit 1
 }
 
-if (!(Test-Path -Path $($image))) { Write-Host "Linking of $($image) FAILED";  exit 1 }
+Start-Process "$($target)-gcc" -ArgumentList "-g -c -o $($bindobj) $($gccargs) --RTS=$($rtspath) $($bindsrc)" -NoNewWindow -Wait
+
+if (!(Test-Path -Path $($bindobj)))
+{
+    Write-Host "Compilation of $($bindsrc) FAILED"; exit 1
+}
+
+$objfiles = $objfiles + $bindobj
+$crt0obj = Join-Path $objdir "crt0.o"
+
+if ($startupgen)
+{
+    $linkerscript = Join-Path $objdir "linker.ld"
+    $crt0src = Join-Path $objdir "crt0.s"
+
+    Start-Process startup-gen -ArgumentList "-P $($gpr) -l $($linkerscript) -s $($crt0src)" -NoNewWindow -Wait
+
+    if (!(Test-Path -Path $($linkerscript)))
+    {
+        Write-Host "Generation of $($linkerscript) FAILED"; exit 1 
+    } else
+    {
+        if (!(Test-Path -Path $($crt0src)))
+        { 
+            Write-Host "Generation of $($crt0src) FAILED"; exit 1 
+        }
+    }
+
+} else
+{
+    $linkerscript = Join-Path $srcdir "linker.ld"
+    if (!(Test-Path -Path $($linkerscript)))
+    {
+        Write-Host "$($linkerscript) is missing"; exit 1
+    } else
+    {
+        $crt0src = Join-Path $srcdir "crt0.s"
+        if (!(Test-Path -Path $($crt0src)))
+        {
+            Write-Host "$($crt0src) is missing"; exit 1
+        }        
+    }
+}
+
+Start-Process "$($target)-as" -ArgumentList "-o $($crt0obj) $($gccargs) $($crt0src)" -NoNewWindow -Wait
+$objfiles = $objfiles + $($crt0obj)
+
+if (!(Test-Path -Path $($crt0obj)))
+{
+    Write-Host "Assembly of $($crt0src) FAILED";  exit 1
+}
+
+Start-Process "$($target)-ld" -ArgumentList "-o $($image) $($objfiles) $($libgnat) -T $($linkerscript)" -NoNewWindow -Wait
+
+if (!(Test-Path -Path $($image)))
+{
+    Write-Host "Linking of $($image) FAILED";  exit 1 
+}
 
 # A fully built target image is available from here onward...
 
